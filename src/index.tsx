fe5c9fb8f7ffc7f5877d439bb4c4fd35c38b5585
rafa <r.salguero@garsalabs.com>
2021-02-23 17:20:05 -0600

    some changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Carousel } from "./carousel";
-
-
+import style from "./style.module.css";
 
 export const App = () => (
-    <div>
-        <Carousel items={[1, 2, 3, 4]} />
+    <div className={style.label}>
+        Hello world
     </div>
 )
 ReactDOM.render(<App />, document.getElementById("root"));
